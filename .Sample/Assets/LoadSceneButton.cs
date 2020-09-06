a3126ffb9ab006019c17288787f5c3636e8c727b
Ricardo Bustamante <ricardo@busta.dev>
2020-09-06 17:35:30 +0200

    Add sample project to help testing stuff.

---
+++ .Sample/Assets/LoadSceneButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class LoadSceneButton : MonoBehaviour
+{
+    public string sceneName;
+    
+    private void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ .Sample/Assets/Scenes/Joystick/MoveObjectWithInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Scenes.Joystick
+{
+    public class MoveObjectWithInput : MonoBehaviour
+    {
+        [SerializeField] private BustaGames.Joystick.Joystick[] joys;
+
+        private void Update()
+        {
+            foreach (var joystick in joys)
+            {
+                transform.position += joystick.Direction * Time.deltaTime;
+            }
+        }
+    }
+}
